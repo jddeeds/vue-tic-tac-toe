d0b88630723abf2d84dfb457457ba625ff960631
Joey Deeds <joey.deeds@gmail.com>
2017-06-03 21:35:14 -0500

    adding strike and created method to cell.vue

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -4,13 +4,30 @@
 
 <script>
 export default {
-    data () {
+    props: ['name'],
+    data() {
         return {
             frozen: false,
             mark: ''
         }
+    },
+    methods: {
+        strike() {
+            if (!this.frozen) {
+                this.mark = this.$parent.activePlayer;
+                this.frozen = true;
+                Event.$emit('strike', this.name);
+            }
+        }
+    },
+        created() {
+            Event.$on('clearCell', () => {
+                this.mark = '';
+                this.frozen = false;
+            })
+            Event.$on('freeze', () => this.frozen = true);
+        }
     }
-}
 </script>
 
 <style>
